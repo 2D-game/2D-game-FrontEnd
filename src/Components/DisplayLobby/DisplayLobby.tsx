5ab4ede15f378008a6248fcade0eddf0968593f4
Tadas Sipavicius <tasipas@gmail.com>
2022-09-19 18:19:09 +0300

    Added displaylobby

--- src/Components/DisplayLobby/DisplayLobby.tsx
+++ src/Components/DisplayLobby/DisplayLobby.tsx
@@ -1,4 +1,4 @@
-import { Box, Typography } from '@mui/material';
+import { Box, Typography, Button } from '@mui/material';
 import React from 'react';
 import { useParams } from 'react-router-dom';
 
@@ -7,9 +7,30 @@ const DisplayLobby = () => {
     let { lobbyid } = useParams();
 
     return (
-        <Box>
-            <Typography>{lobbyid}</Typography>
-        </Box>
+        <Box sx={{ width: "100%" }}>
+            <Box sx={{ display: "flex", flexDirection: "row", marginTop: 2, justifyContent: "center" }}>
+                <Box>
+                    Lobby ID:
+                </Box>
+                <Box sx={{ fontWeight: 600, marginLeft: 1 }}>
+                    {lobbyid}
+                </Box>
+            </Box>
+
+            <Box sx={{ marginTop: 2, textAlign: "center" }}>
+                <Typography>
+                    Joined Users In Lobby:
+                </Typography>
+            </Box>
+
+            <Box sx={{ textAlign: "center", marginTop: 1 }}>
+                <Button>
+                    Ready To Start
+                </Button>
+            </Box>
+
+        </Box >
+
     )
 }
 
