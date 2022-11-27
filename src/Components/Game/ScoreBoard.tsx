9adb7d9a09fc11512e57258d7917d052abe19ba9
WaScript3r <wascript3r@gmail.com>
2022-11-27 16:40:00 +0200

    sorted scores

--- src/Components/Game/ScoreBoard.tsx
+++ src/Components/Game/ScoreBoard.tsx
@@ -1,6 +1,7 @@
 import { StyledScoreBoard } from './Game.style'
 
 export const ScoreBoard = ({ scores }: { scores: any }) => {
+	const sortedScores = scores.sort((a: any, b: any) => b.score - a.score)
 	return (
 		<StyledScoreBoard>
 			<h3>Score board</h3>
@@ -9,7 +10,7 @@ export const ScoreBoard = ({ scores }: { scores: any }) => {
 					<th>Username</th>
 					<th>Score</th>
 				</tr>
-				{scores.map((score: any) => (
+				{sortedScores.map((score: any) => (
 					<tr key={score.id}>
 						<td>{score.username}</td>
 						<td>{score.score}</td>
