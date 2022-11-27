bde604e3029fbdadb39e5f5bd0b29a5bfa63f151
WaScript3r <wascript3r@gmail.com>
2022-11-27 16:40:43 +0200

    added position

--- src/Components/Game/ScoreBoard.tsx
+++ src/Components/Game/ScoreBoard.tsx
@@ -7,11 +7,13 @@ export const ScoreBoard = ({ scores }: { scores: any }) => {
 			<h3>Score board</h3>
 			<table style={{ margin: '0 auto' }}>
 				<tr>
+					<th>Position</th>
 					<th>Username</th>
 					<th>Score</th>
 				</tr>
-				{sortedScores.map((score: any) => (
+				{sortedScores.map((score: any, index: number) => (
 					<tr key={score.id}>
+						<td>{index + 1}</td>
 						<td>{score.username}</td>
 						<td>{score.score}</td>
 					</tr>
